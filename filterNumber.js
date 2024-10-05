c4c4cefc75b5a50bebeef585f4285a4d8e33793a
Harshitha <chharshitha277@gmail.com>
2024-10-05 16:37:41 +0530

    "task 5 js completed backend"

--- filterNumber.js
+++ filterNumber.js
@@ -1,1 +1,25 @@
 
+
+function filterNumbers(arr, criterion) {
+    switch(criterion) {
+        case 'even':
+            return arr.filter(num => num % 2 === 0);
+      
+        case 'odd':
+            return arr.filter(num => num % 2 !== 0);
+    
+        case 'negetive':
+            return arr.filter(num=> num<0);
+    
+        case 'positive':
+            return arr.filter(num=>num>0);
+        default:
+            return arr;
+    }
+}
+console.log(filterNumbers([1, 2, 3, 4, 5, 6], 'even')); 
+console.log(filterNumbers([1, 2, 3, 4, 5, 6], 'odd')); 
+console.log(filterNumbers([-1, -2, 3, 4, -5, 6], 'negative')); 
+console.log(filterNumbers([-1, -2, 3, 4, -5, 6], 'positive')); 
+
+
